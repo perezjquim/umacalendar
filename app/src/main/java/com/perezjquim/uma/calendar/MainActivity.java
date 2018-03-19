88b79bc7f482327a3a386ebe1c083be0a31b4a19
Joaquim Perez <jquim1@hotmail.com>
2018-03-19 21:36:51 +0000

    Initial layout

--- app/src/main/java/com/perezjquim/uma/calendar/MainActivity.java
+++ app/src/main/java/com/perezjquim/uma/calendar/MainActivity.java
@@ -1,13 +1,26 @@
 package com.perezjquim.uma.calendar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private TextView console;
+    private TextView field;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        console = findViewById(R.id.console);
+        field = findViewById(R.id.field);
+    }
+    public void requestCalendar(View v)
+    {
+        //ICalendar ical = Biweekly.parse(file).first();
+        //console.setText(ical.writeJson());
     }
 }
