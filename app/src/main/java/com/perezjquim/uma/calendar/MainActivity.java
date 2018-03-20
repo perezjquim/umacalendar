b2658af543c0788f8594599501b35610148d5738
Joaquim Perez <jquim1@hotmail.com>
2018-03-20 18:02:45 +0000

    'Load previous calendar' option

--- app/src/main/java/com/perezjquim/uma/calendar/MainActivity.java
+++ app/src/main/java/com/perezjquim/uma/calendar/MainActivity.java
@@ -58,4 +58,9 @@ public class MainActivity extends AppCompatActivity
             }
         }).start();
     }
+
+    public void loadPreviousCalendar(View v)
+    {
+        startActivity( new Intent(this,ResultsActivity.class));
+    }
 }
